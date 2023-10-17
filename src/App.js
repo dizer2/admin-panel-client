b8e7d032e3873b49df3c81f3ff8312e6c5dcda05
Yurii <fanfjrfj589@gmail.com>
2023-10-17 08:39:01 +0200

    add register form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./compontnes/register/Register";
 import Login from "./compontnes/login/Login";
+import Home from "./compontnes/home/Home";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/*" element={<Register />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
 	</BrowserRouter>
   );
--- src/compontnes/register/Register.jsx
+++ src/compontnes/register/Register.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style/Register.css"
 import Buttons from '../UI/buttons/Buttons';
+import { useNavigate } from 'react-router-dom';
 
 
 const Register = () => {
+
+	const [login, thisLogin] = useState('');
+	const [password, thisPassword] = useState('');
+
+	const navigate = useNavigate();
+
+
+
+	const signUp = async () => {
+		console.log(login);
+		console.log(password);
+		try {
+			const response = await fetch('http://localhost:5000/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				login: login,
+				password: password,
+			}),
+			});
+	
+			if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+	
+		
+			const result = await response.json();
+			console.log(result);
+			navigate("/home");
+		} catch (error) {
+			console.error("Error:", error);
+		}
+	}
+
+
   return (
 	<div className="register">
 		<div className="register__form">
@@ -11,19 +49,30 @@ const Register = () => {
 
 			<div className="register__form-box">
 				<p className='register__input-title'>Come up with your own unique name</p>
-				<input className="register__input" />
+				<input 
+					 onChange={(e) => {
+						thisLogin(e.target.value);
+					  }}   
+					value={login}
+					className="register__input" 
+				/>
 			</div>
 
 			<div className="register__form-box">
 				<p className='register__input-title'>Come up with your own unique name</p>
-				<input className="register__input" />
+				<input 
+					 onChange={(e) => {
+						thisPassword(e.target.value);
+					  }}   
+					value={password}
+					className="register__input" />
 			</div>
 
 			<div className="register__form-signUp">
 				<p className='register__signUp'>Do you already have an existing account? <span>Sign in</span></p>
 			</div>
 			
-			<div className="register__form-button">
+			<div className="register__form-button" onClick={signUp}>
 				<Buttons text="Sign up" width={450} height={80} size={24}/>
 			</div>
 		</div>
